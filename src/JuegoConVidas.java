837193bb7a92a7fa0220bbab7ee35643ce42291a
jorge <91345902+jreyesgfi@users.noreply.github.com>
2024-10-07 19:05:34 +0300

    first commit

---
+++ src/JuegoConVidas.java
@@ -0,0 +1,68 @@
+/**
+ * Clase para gestionar las vidas y el registro de récords en el juego "JuegoConVidas".
+ * Esta clase proporciona métodos para manejar el estado del juego, incluyendo el conteo de vidas y el seguimiento del récord más alto.
+ */
+public class JuegoConVidas {
+
+    private int vidas;
+    private int vidas_iniciales;
+    private static int record = 0;
+
+    /**
+     * Construye un nuevo juego con un número especificado de vidas iniciales.
+     * @param numeroVidasIniciales el número de vidas con las que el jugador comienza.
+     */
+    public JuegoConVidas(int numeroVidasIniciales) {
+        this.vidas_iniciales = numeroVidasIniciales;
+        this.vidas = numeroVidasIniciales;
+    }
+
+    /**
+     * Muestra el número inicial de vidas para este juego.
+     */
+    public void muestraVidasIniciales() {
+        System.out.println("Vidas iniciales: " + this.vidas_iniciales);
+    }
+
+    /**
+     * Muestra el número de vidas restantes para el jugador.
+     */
+    public void muestraVidasRestantes() {
+        System.out.println("Vidas restantes: " + this.vidas);
+    }
+
+    /**
+     * Decrementa el número de vidas en uno. Imprime las vidas actuales restantes si las hay,
+     * de lo contrario imprime "Juego Terminado".
+     * @return true si aún quedan vidas, false de lo contrario.
+     */
+    public boolean quitaVida() {
+        this.vidas--;
+        if (this.vidas > 0) {
+            System.out.println("Vidas restantes: " + this.vidas);
+            return true;
+        }
+        System.out.println("Juego Terminado");
+        return false;
+    }
+
+    /**
+     * Reinicia el juego al número inicial de vidas.
+     */
+    public void reiniciaPartida() {
+        this.vidas = this.vidas_iniciales;
+    }
+
+    /**
+     * Actualiza el récord de vidas si las vidas restantes actuales superan el récord.
+     * Muestra un mensaje cuando se alcanza o se supera el récord.
+     */
+    public void actualizaRecord() {
+        if (this.vidas == record) {
+            System.out.println("Record de vidas alcanzado con " + this.vidas + " vidas");
+        } else if (this.vidas > record) {
+            record = this.vidas;
+            System.out.println("Record de vidas batido, el nuevo record es: " + this.vidas);
+        }
+    }
+}
