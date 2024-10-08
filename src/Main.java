50625588fa5d384f44f561f00c73ebb600ded578
jorge <91345902+jreyesgfi@users.noreply.github.com>
2024-10-08 23:40:51 +0300

    Implemented method jugar

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,29 @@ import Entrada.MyInput;
 
 public class Main {
     public static void main(String[] args) {
-        JuegoAdivinaNumero juego1 = new JuegoAdivinaNumero(10,5);
-        juego1.reiniciaPartida();
+        // Parámetros del juego
+        int vidas = 0;
+        int numeroAdivinar = 3;
+        int numeroAdivinarPar = 8;
+        int numeroAdivinarImpar = 5;
+
+        // Creamos los juegos
+        JuegoAdivinaNumero juegoAdivinaNumero = new JuegoAdivinaNumero(vidas, numeroAdivinar);
+        JuegoAdivinaNumeroPar juegoAdivinaNumeroPar = new JuegoAdivinaNumeroPar(vidas, numeroAdivinarPar);
+        JuegoAdivinaNumeroImpar juegoAdivinaNumeroImpar = new JuegoAdivinaNumeroImpar(vidas, numeroAdivinarImpar);
+
+        // Jugamos
+        jugar(juegoAdivinaNumero);
+        jugar(juegoAdivinaNumeroPar);
+        jugar(juegoAdivinaNumeroImpar);
+
+    }
+    static void jugar(JuegoConVidas juego){
+        juego.reiniciaPartida();
         boolean jugando = true;
         while (jugando){
             String intento = MyInput.readString();
-            jugando = juego1.juega(intento);
+            jugando = juego.juega(intento);
         }
     }
 }
